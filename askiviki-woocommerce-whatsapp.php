bb995dc8079f961420aa1a59b52995971ba01b66
Ask I Viki <askiviki.official@gmail.com>
2026-06-13 23:30:11 +0530

    Initial plugin bootstrap

---
+++ askiviki-woocommerce-whatsapp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Ask I Viki WooCommerce WhatsApp
+ * Plugin URI: https://askiviki.simpletechgroups.com
+ * Description: Send WooCommerce order notifications through WhatsApp.
+ * Version: 1.0.0
+ * Author: Vigneshwaran P
+ * Author URI: https://askiviki.simpletechgroups.com
+ * License: GPL v2 or later
+ * Text Domain: askiviki-whatsapp
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
